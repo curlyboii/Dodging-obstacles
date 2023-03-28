6f03490842eac031d767c60cd3acf874c99acb20
Vitalii <vetalsofyk@gmail.com>
2023-03-28 16:39:24 +0300

    Enemy
    Made it so that the obstacle on us moved, and collapsed behind the camera -10f, so as not to clog the scene

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0,0, speed * Time.deltaTime);
+
+        if (transform.position.z < -10f)
+        { 
+        
+            Destroy(gameObject);
+
+        }
+        
+    }
+}

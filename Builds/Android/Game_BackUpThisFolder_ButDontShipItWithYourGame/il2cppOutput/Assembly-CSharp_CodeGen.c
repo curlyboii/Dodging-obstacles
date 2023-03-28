6292c819b52b9f033408158b0c79c3367f165a3e
Vitalii <vetalsofyk@gmail.com>
2023-03-29 00:44:17 +0300

    Builds

---
+++ Builds/Android/Game_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,129 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void Enemy::Start()
+extern void Enemy_Start_m8BBD9A5AE10A27ABDFCD9168B93CD9C69D229034 (void);
+// 0x00000002 System.Void Enemy::Update()
+extern void Enemy_Update_m4149CFC3AC081AF0D654D9BDB6BC9B5540CE03D8 (void);
+// 0x00000003 System.Void Enemy::.ctor()
+extern void Enemy__ctor_mB6697627910F785A971C20C671DEFBA9D921D933 (void);
+// 0x00000004 System.Void GameManager::Awake()
+extern void GameManager_Awake_m4B6E8E2AF58C95C9A2A0C4637A34AE0892CB637F (void);
+// 0x00000005 System.Void GameManager::Start()
+extern void GameManager_Start_m87A71D65F3171A58DBDDBFB03832ADA65643D0E2 (void);
+// 0x00000006 System.Void GameManager::Update()
+extern void GameManager_Update_m7F29D8E933B8D21D2E67507979C0F12ACF87BB41 (void);
+// 0x00000007 System.Collections.IEnumerator GameManager::SpawnEnemies()
+extern void GameManager_SpawnEnemies_mC5A46E8E1E7EFADD192B79073C4A7ABE0AAB73ED (void);
+// 0x00000008 System.Void GameManager::Spawn()
+extern void GameManager_Spawn_m6A827C8AFB713CB38586058AF18FEADBC16906DA (void);
+// 0x00000009 System.Void GameManager::Restart()
+extern void GameManager_Restart_mCAD4B8EB332D65D94F493AD96956EC1777DC3854 (void);
+// 0x0000000A System.Void GameManager::ScoreUp()
+extern void GameManager_ScoreUp_m0F05B0D8F27C3810C3A6D62FFCF3B81EE2238DC7 (void);
+// 0x0000000B System.Void GameManager::.ctor()
+extern void GameManager__ctor_mF453CED520617BFB65C52405A964E06CF17DB368 (void);
+// 0x0000000C System.Void GameManager/<SpawnEnemies>d__13::.ctor(System.Int32)
+extern void U3CSpawnEnemiesU3Ed__13__ctor_m4476528D39321BE8B576F74E0ADFF38F79A6AC3F (void);
+// 0x0000000D System.Void GameManager/<SpawnEnemies>d__13::System.IDisposable.Dispose()
+extern void U3CSpawnEnemiesU3Ed__13_System_IDisposable_Dispose_mF4D8BFCE10F319119184E2FEC311404624205A4B (void);
+// 0x0000000E System.Boolean GameManager/<SpawnEnemies>d__13::MoveNext()
+extern void U3CSpawnEnemiesU3Ed__13_MoveNext_mE7875DDCC8470063855CD641EECC47BF314D28D6 (void);
+// 0x0000000F System.Object GameManager/<SpawnEnemies>d__13::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CSpawnEnemiesU3Ed__13_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mC670550C345BD2F506BB79D4A25471040B445680 (void);
+// 0x00000010 System.Void GameManager/<SpawnEnemies>d__13::System.Collections.IEnumerator.Reset()
+extern void U3CSpawnEnemiesU3Ed__13_System_Collections_IEnumerator_Reset_m563C9D7C150AD84A2E151AE96FD1DFB8F2CF4CCE (void);
+// 0x00000011 System.Object GameManager/<SpawnEnemies>d__13::System.Collections.IEnumerator.get_Current()
+extern void U3CSpawnEnemiesU3Ed__13_System_Collections_IEnumerator_get_Current_m7A7E5C5E667A564D36E45E272747C9889B18876B (void);
+// 0x00000012 System.Void Player::Start()
+extern void Player_Start_mC31CF4F40DDEA35C5E39E8C43EC37284AE7C453D (void);
+// 0x00000013 System.Void Player::Update()
+extern void Player_Update_m95E134A5EF1B5164EA281F61D7FA436F59BE3C9F (void);
+// 0x00000014 System.Void Player::TouchInput()
+extern void Player_TouchInput_m9195E0780988B7DB30284A5702767F37695FBA1C (void);
+// 0x00000015 System.Void Player::OnTriggerEnter(UnityEngine.Collider)
+extern void Player_OnTriggerEnter_m51AE06A67257AFF378594339C9D53201B6104650 (void);
+// 0x00000016 System.Void Player::.ctor()
+extern void Player__ctor_m0A83E0706592FC871B0CF188B37AFC6649F3D85D (void);
+static Il2CppMethodPointer s_methodPointers[22] = 
+{
+	Enemy_Start_m8BBD9A5AE10A27ABDFCD9168B93CD9C69D229034,
+	Enemy_Update_m4149CFC3AC081AF0D654D9BDB6BC9B5540CE03D8,
+	Enemy__ctor_mB6697627910F785A971C20C671DEFBA9D921D933,
+	GameManager_Awake_m4B6E8E2AF58C95C9A2A0C4637A34AE0892CB637F,
+	GameManager_Start_m87A71D65F3171A58DBDDBFB03832ADA65643D0E2,
+	GameManager_Update_m7F29D8E933B8D21D2E67507979C0F12ACF87BB41,
+	GameManager_SpawnEnemies_mC5A46E8E1E7EFADD192B79073C4A7ABE0AAB73ED,
+	GameManager_Spawn_m6A827C8AFB713CB38586058AF18FEADBC16906DA,
+	GameManager_Restart_mCAD4B8EB332D65D94F493AD96956EC1777DC3854,
+	GameManager_ScoreUp_m0F05B0D8F27C3810C3A6D62FFCF3B81EE2238DC7,
+	GameManager__ctor_mF453CED520617BFB65C52405A964E06CF17DB368,
+	U3CSpawnEnemiesU3Ed__13__ctor_m4476528D39321BE8B576F74E0ADFF38F79A6AC3F,
+	U3CSpawnEnemiesU3Ed__13_System_IDisposable_Dispose_mF4D8BFCE10F319119184E2FEC311404624205A4B,
+	U3CSpawnEnemiesU3Ed__13_MoveNext_mE7875DDCC8470063855CD641EECC47BF314D28D6,
+	U3CSpawnEnemiesU3Ed__13_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mC670550C345BD2F506BB79D4A25471040B445680,
+	U3CSpawnEnemiesU3Ed__13_System_Collections_IEnumerator_Reset_m563C9D7C150AD84A2E151AE96FD1DFB8F2CF4CCE,
+	U3CSpawnEnemiesU3Ed__13_System_Collections_IEnumerator_get_Current_m7A7E5C5E667A564D36E45E272747C9889B18876B,
+	Player_Start_mC31CF4F40DDEA35C5E39E8C43EC37284AE7C453D,
+	Player_Update_m95E134A5EF1B5164EA281F61D7FA436F59BE3C9F,
+	Player_TouchInput_m9195E0780988B7DB30284A5702767F37695FBA1C,
+	Player_OnTriggerEnter_m51AE06A67257AFF378594339C9D53201B6104650,
+	Player__ctor_m0A83E0706592FC871B0CF188B37AFC6649F3D85D,
+};
+static const int32_t s_InvokerIndices[22] = 
+{
+	3449,
+	3449,
+	3449,
+	3449,
+	3449,
+	3449,
+	3357,
+	3449,
+	3449,
+	3449,
+	3449,
+	2793,
+	3449,
+	3297,
+	3357,
+	3449,
+	3357,
+	3449,
+	3449,
+	3449,
+	2811,
+	3449,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	22,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
